5c9ddc619104e9046785de4eacccb3e8482f92b7
Ankush <ankushdhar20@gmail.com>
2023-08-26 23:46:39 +0530

    ValidAnagram

--- src/com/company/ValidAnagram.java
+++ src/com/company/ValidAnagram.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class ValidAnagram {
     public static boolean isAnagram (String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
